0b1dd7023199b46447e8f567edec9c96cc7d0c1e
Dejan22 <dejan.tomasevic@protonmail.com>
2017-12-25 17:06:48 +0100

    Created viuw for artists

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,11 @@
 class Artist < ApplicationRecord
+  has_many :songs, dependent: :destroy
+
+  def self.order_by_name
+    order(:name)
+  end
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :biography, presence: true, length: { maximum: 1000 }
+  validates :image_url, presence: true
 end
