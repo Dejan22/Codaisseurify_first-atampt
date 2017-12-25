643d1d6e3bd736f6d817631d77b53cd7f200b8e7
Dejan22 <dejan.tomasevic@protonmail.com>
2017-12-25 18:48:16 +0100

    Index page for artist and show page for artist created

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 artis1 = Artist.create!( { name: "Melody Gardot", description: "De zangeres Melody Gardot overwon een aantal grote tegenslagen in haar leven. Ze werd in 1985 geboren in New Jersey en leerde pianospelen. Ze was als artieste actief in de nachtclubwereld van Philadelphia.",  image_url: 'http://res.cloudinary.com/dpxrgcpfs/image/upload/v1514202486/0060254729193_600_xudepq.jpg' } )
 artist2 = Artist.create!( { name: "Toledo", description: "It plays as acid-jazz noir, in which cocktails, call girls and varied psychoses come together in music that haunts as it swings. Risky, but Toledos dynamic delivery, the strength of his neo Beat wordplay and the sublime excellence of his backing jazzmen make sure that musical substance outpaces the sharp style.", image_url: 'http://res.cloudinary.com/dpxrgcpfs/image/upload/v1514202479/aa1c2a67d82f406ec0fdad8494ce515b_illiws.png' } )
+
+
 song_1 = Song.create!(
   name: "Don't Misunderstand",
   track: 1,
