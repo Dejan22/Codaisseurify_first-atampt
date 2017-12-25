8003fbd6d14fd408bf47a42b46bc5e26e5d87d78
Dejan22 <dejan.tomasevic@protonmail.com>
2017-12-25 16:33:24 +0100

    models, migration, seeds finished with data filled

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+artis1 = Artist.create!( { name: "Melody Gardot", description: "De zangeres Melody Gardot overwon een aantal grote tegenslagen in haar leven. Ze werd in 1985 geboren in New Jersey en leerde pianospelen. Ze was als artieste actief in de nachtclubwereld van Philadelphia.",  image_url: 'http://res.cloudinary.com/dpxrgcpfs/image/upload/v1514202486/0060254729193_600_xudepq.jpg' } )
+artist2 = Artist.create!( { name: "Toledo", description: "It plays as acid-jazz noir, in which cocktails, call girls and varied psychoses come together in music that haunts as it swings. Risky, but Toledos dynamic delivery, the strength of his neo Beat wordplay and the sublime excellence of his backing jazzmen make sure that musical substance outpaces the sharp style.", image_url: 'http://res.cloudinary.com/dpxrgcpfs/image/upload/v1514202479/aa1c2a67d82f406ec0fdad8494ce515b_illiws.png' } )
+song_1 = Song.create!(
+  name: "Don't Misunderstand",
+  track: 1,
+
+  # artist: melody_gardot
+)
+
+song_2 = Song.create!(
+  name: "Don't Talk",
+  track: 2,
+
+  # artist: melody_gardot
+)
+
+song_3 = Song.create!(
+  name: "It Gonna Come",
+  track: 3,
+
+  # artist: melody_gardot
+)
+
+song_4 = Song.create!(
+  name: "Death Took a Holiday",
+  track: 1,
+
+  # artist: toledo
+)
+
+song_5 = Song.create!(
+  name: "Danny",
+  track: 2,
+
+  # artist: toledo
+)
