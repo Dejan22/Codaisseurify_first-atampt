9e83f1e324466d237e69df621e61c336a3d22c02
Dejan22 <dejan.tomasevic@protonmail.com>
2017-12-25 20:09:28 +0100

    edit delete update of artist finished complitely

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   root "artists#index"
-
-  get "artists" => "atrists#index"
-  get 'artists/new' => "artists#new", as: :new_artist
-  get "artists/:id" => "artists#show", as: :artist
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :artists
 end
