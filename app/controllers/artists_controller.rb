046d8e225c0e3b6f0bc6b1e94cf22551d2817c7a
Dejan22 <dejan.tomasevic@protonmail.com>
2017-12-25 18:55:14 +0100

    New model, controler and viuw for artist created

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,4 +5,7 @@ class ArtistsController < ApplicationController
   def show
     @artist = Artist.find(params[:id])
   end
+  def new
+    @artist = Artist.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "artists#index"
 
   get "artists" => "atrists#index"
+  get 'artists/new' => "artists#new", as: :new_artist
   get "artists/:id" => "artists#show", as: :artist
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
